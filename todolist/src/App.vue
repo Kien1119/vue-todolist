c78b2964e9d97df3a31f0596e18ef0dc8a185ae1
Kienpham <Phamtrungkien062003@gmail.com>
2024-07-22 14:32:00 +0700

    vue-todolist

---
+++ todolist/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <TaskTodo msg="📋 My tasks" />
+</template>
+
+<script>
+import TaskTodo from "./components/TaskTodo.vue";
+import "primeicons/primeicons.css";
+export default {
+  name: "App",
+  components: {
+    TaskTodo,
+  },
+};
+</script>
+
+<style>
+body {
+  /* background: #222; */
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+
+.new-task-form {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  color: white;
+  background: #333;
+  padding: calc(2 * 0.5rem);
+  border-radius: calc((0.5rem));
+  width: calc(100% - 4 * 0.5rem);
+  max-width: 50rem;
+  border: 1px solid #333;
+  margin-left: calc(30% - 4 * 0.5rem);
+}
+.new-task-form:hover {
+  background-color: #444;
+  border: 1px solid white;
+}
+#new-task {
+  width: 568px;
+  height: 40px;
+  position: relative;
+  opacity: 0;
+  padding: 0.5rem;
+  flex: 1;
+  color: white;
+  border-radius: 10px;
+  margin-right: 10px;
+  margin-left: 10px;
+}
+#new-task:focus {
+  opacity: 1;
+  outline: none;
+  background: #333;
+  border: none;
+}
+
+.submit {
+  border: none;
+  margin: 0;
+  padding: 0;
+  background: transparent;
+  font-family: inherit;
+  font-size: inherit;
+}
+button {
+  float: right;
+  margin-left: 0;
+  margin-right: 22%;
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 1em;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  background: var(--task-background);
+  color: white;
+}
+</style>
---
+++ todolist/src/components/TaskTodo.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="task">
+      <InputText type="text" v-model="valueTest" />
+      <h1>{{ msg }}</h1>
+      <label for="new-task"></label>
+      <div class="tasks">
+        <div class="new-task-form">
+          <label for="new-task-form">➕ Add a task</label>
+          <input
+            type="text"
+            name=""
+            id="new-task"
+            v-model="newTask"
+            @:keyup.enter="addTask"
+            placeholder="Try typing 'Buy milk'"
+            autocapitalize="off"
+          />
+          <input type="submit" class="submit" value="➡️" />
+          <ul>
+            <li v-for="(task, index) in tasks" :key="index">
+              <input type="checkbox" v-model="index.completed" />
+              <span :class="{ completed: task.completed }">{{
+                task.text
+              }}</span>
+              <i class="pi pi-search" style="font-size: 2rem; color: black"></i>
+              <button @click="deleteTask(index)">Clear All Tasks</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <button @click="clearCompleted">Clear Completed</button>
+  </div>
+</template>
+
+<script>
+import InputText from "primevue/inputtext";
+import { ref } from "vue";
+
+export default {
+  name: "HelloWorld",
+  components: {
+    InputText,
+  },
+  props: {
+    msg: String,
+    value: String,
+  },
+  setup() {
+    const valueTest = ref();
+    return valueTest;
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
